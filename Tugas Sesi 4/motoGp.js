b5796e83e3061ab5f986eb4c959354bf75e7ce1d
hrlaan12 <ujang.herlan_ti22@nusaputra.ac.id>
2023-10-27 14:17:23 +0700

    git commit -m "frist commit"

--- Tugas Sesi 4/motoGp.js
+++ Tugas Sesi 4/motoGp.js
@@ -1,50 +1,147 @@
-var motoGP =[
-    {
-        circuit:'Losail',
-        location:'Qatar',
-        winner:{
-            fristName:'Andrea',
-            lastName:'Dovizioso',
-            country:'Italy'
-        }
-    },
-    {
-        circuit:'Autodromo',
-        location:'Argentina',
-        winner:{
-            fristName:'Cal',
-            lastName:'Crutchlow',
-            country:'UK'
-        }
-    },
-    {
-        circuit:'De Jerez',
-        location:'Spain',
-        winner:{
-            fristName:'Valentino',
-            lastName:'Rossi',
-            country:'Italy'
-        }
-    },
-    {
-        circuit:'Mugello',
-        location:'Italy',
-        winner:{
-            fristName:'Andrea',
-            lastName:'Devezioso',
-            country:'Italy'
-        }
-    },
-]
-var result = motoGP.reduce((acc, race) => {
-    const { circuit, location, winner } = race;
-    const { country, firstName, lastName } = winner;
+const express = require('express');
+const app = express();
+const port = 8000;
 
-    acc[country] = acc[country] || { winningCircuits: [], totalWin: 0 };
-    acc[country].winningCircuits.push({ name: `${firstName} ${lastName}`, winLocation: `${circuit}, ${location}` });
-    acc[country].totalWin++;
+const motoGP = [
+    {
+        circuit: 'Losail',
+        location: 'Qatar',
+        winner: {
+            firstName: 'Andrea',
+            lastName: 'Dovizioso',
+            country: 'Italy'
+        }
+    },
+    {
+        circuit: 'Autodromo',
+        location: 'Argentine',
+        winner: {
+            firstName: 'Cal',
+            lastName: 'Crutchlow',
+            country: 'Uk'
+        }
+    },
+    {
+        circuit: 'De Jerez',
+        location: 'Spain',
+        winner: {
+            firstName: 'Valentino',
+            lastName: 'Rossi',
+            country: 'Italy'
+        }
+    },
+    {
+        circuit: 'Mugello',
+        location: 'Italy',
+        winner: {
+            firstName: 'Andrea',
+            lastName: 'Dovizioso',
+            country: 'Italy'
+        }
+    }
+];
 
-    return acc;
-}, {});
+const groupedData = {};
 
-console.log(JSON.stringify(result, null, 2));
+motoGP.forEach((race) => {
+    const winner = race.winner;
+    const winnerKey = winner.country;
+
+    if (!groupedData[winnerKey]) {
+        groupedData[winnerKey] = {
+            winningCircuits: [],
+            totalWin: 0
+        };
+    }
+
+    groupedData[winnerKey].winningCircuits.push({
+        name: winner.firstName + ' ' + winner.lastName,
+        winLocation: race.circuit + ', ' + race.location
+    });
+
+    groupedData[winnerKey].totalWin++;
+
+});
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+            route: '/winners/country',
+            description: 'Tampilkan pemenang berdasarkan negara',
+        },
+        {
+            route: '/winners/name',
+            description: 'Tampilkan pemenang berdasarkan nama',
+        },
+        {
+            route: '/winners/circuit',
+            description: 'Tampilkan pemenang berdasarkan sirkuit',
+        }
+    ]);
+});
+
+
+app.get('/winners/country', (req, res) => {
+    const winners = {};
+
+    motoGP.forEach((race) => {
+        const winner = race.winner;
+        const country = winner.country;
+
+        if (!winners[country]) {
+            winners[country] = {
+                winningCircuits: [],
+                totalWin: 0
+            };
+        }
+
+        winners[country].winningCircuits.push({
+            name: winner.firstName + ' ' + winner.lastName,
+            winLocation: race.circuit + ', ' + race.location
+        });
+
+        winners[country].totalWin++;
+    });
+
+    res.json(winners);
+});
+
+
+app.get('/winners/name', (req, res) => {
+    const allWinners = [];
+    let totalWin = 0;
+
+    for (const country in groupedData) {
+        groupedData[country].winningCircuits.forEach((winner) => {
+            allWinners.push({ name: winner.name });
+            totalWin++;
+        });
+    }
+
+    if (allWinners.length > 0) {
+        res.json({ winners: allWinners, totalWin });
+    } else {
+        res.status(404).json({ message: 'Nama pemenang tidak ditemukan' });
+    }
+});
+
+app.get('/winners/circuit', (req, res) => {
+    const allCircuits = [];
+    for (const country in groupedData) {
+        groupedData[country].winningCircuits.forEach((winner) => {
+            const circuitName = winner.winLocation.split(', ')[0]; // Mengambil nama sirkuit
+            allCircuits.push({ country, name: winner.name,circuit: circuitName });
+        });
+    }
+
+    if (allCircuits.length > 0) {
+        res.json(allCircuits);
+    } else {
+        res.status(404).json({ message: 'Sirkuit tidak ditemukan' });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`);
+});
